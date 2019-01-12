494d51c5f7374a8923506a289bd42277693a7511
mahmoudabdelaziz1993 <mahmoudabdelaziz1993@outlook.com>
2019-01-13 00:22:47 +0200

    facebook auth v 1

--- keys/index.js
+++ keys/index.js
@@ -2,7 +2,7 @@ module.exports = {
     google: {
         clientID: "1259843080838215",
         clientSecret: "06db3aa4fb04f3995cd022c928db8e0c",
-        callbackURL: "https://thawing-sierra-46086.herokuapp.com/auth/facebook/callback"
+        callbackURL: "/auth/facebook/callback"
     },
     mongo: {
         url: "mongodb://phantom:123456a@ds145183.mlab.com:45183/psychic_circle"
